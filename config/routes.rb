34dcef631ddf359afa074e11c965ce4decd4351c
Daisuke Minagawa <iamworkingwell@gmail.com>
2019-01-11 10:29:49 +0900

    first commit

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,4 @@ Rails.application.routes.draw do
   resources :questions do
     resources :answers
   end
-
-  # get '/', to: 'questions#index'
-  # get 'questions/show'
-  # get 'questions/new'
-  # get 'questions/edit'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
